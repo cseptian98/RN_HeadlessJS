3e0f0f808e378a4d2b2714603f14eea7eee779cc
Chandra Septian <cst_dev155@unitedtractors.com>
2021-10-07 08:30:13 +0700

    Update with constant and promise

--- App.js
+++ App.js
@@ -20,9 +20,23 @@ import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 
 const {CalendarModule} = NativeModules;
 
-const onPress = () => {
-  console.log('We will invoke you the native module here!');
-  CalendarModule.createCalendarEvent('testName', 'testLocation');
+const {DEFAULT_EVENT_NAME} = CalendarModule.getConstants();
+console.log(DEFAULT_EVENT_NAME);
+
+const onPress = async () => {
+  try {
+    const eventId = await CalendarModule.createCalendarEvent(
+      'Party',
+      'My House',
+    );
+    console.log(`Create a new event with id ${eventId}`);
+  } catch (err) {
+    console.error(err);
+  }
+  // console.log('We will invoke you the native module here!');
+  // CalendarModule.createCalendarEvent('testName', 'testLocation', eventId => {
+  //   console.log(`Created event with id ${eventId}`);
+  // });
 };
 
 const App = () => {
--- android/app/src/main/java/com/rn_headlessjs/CalendarModule.java
+++ android/app/src/main/java/com/rn_headlessjs/CalendarModule.java
@@ -1,7 +1,9 @@
 package com.rn_headlessjs; // replace com.your-app-name with your app’s name
 import android.util.Log;
 
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -19,8 +21,19 @@ public class CalendarModule extends ReactContextBaseJavaModule {
         return "CalendarModule";
     }
 
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put("DEFAULT_EVENT_NAME", "New Event");
+        return constants;
+    }
+
     @ReactMethod
-    public void createCalendarEvent(String name, String location) {
-        Log.d("CalendarModule", "Create event called with name: " + name + " and location: " + location);
+    public void createCalendarEvent(String name, String location, Promise promise) {
+        try {
+            Integer eventId = 387;
+            promise.resolve(eventId);
+        } catch (Exception e) {
+            promise.reject("Create Event Error", e);
+        };
     }
 }
